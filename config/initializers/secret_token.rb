c47a16056e13b583d68541ddd5c98d4552876df5
Invar Gävert <ingvar@gavert.net>
2013-04-01 22:42:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collectrix::Application.config.secret_token = 'c9c0e143cda2e2350590626d5c54cd9c52543991c70c754018ce0ea2dfac89a4b0b959180b2956c47383c2c579cd28488919a0a1ac1bd5896b9b64dd85ab9448'
